19edc4ffcf24d5cb211c88e21c7d0ef825973122
Endre Márk Borza <endremborza@gmail.com>
2021-03-15 11:10:23 +0100

    add contextman

--- branthebuilder/release.py
+++ branthebuilder/release.py
@@ -19,10 +19,9 @@ def new(c):
 
 @task
 def tag(c):
-    f = io.StringIO()
-    c.run("git rev-parse --abbrev-ref HEAD", out_stream=f)
-    branch = f.getvalue().strip()
-    f.close()
+    with io.StringIO() as f:
+        c.run('git rev-parse --abbrev-ref HEAD', out_stream=f)
+        branch = f.getvalue().strip()
 
     if branch not in ["master", "main"]:
         print("only master/main branch can be tagged")
